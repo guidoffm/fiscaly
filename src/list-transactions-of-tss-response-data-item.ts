630b1e28845e4c239a2c89f65bf5c7a8dd496abd
guidoffm <guidoffm@users.noreply.github.com>
2022-01-30 18:42:37 +0100

    Added missing properteis to ListTransactionsOfTssResponseDataItem

--- src/list-transactions-of-tss-response-data-item.ts
+++ src/list-transactions-of-tss-response-data-item.ts
@@ -1,4 +1,4 @@
-import { Schema } from ".";
+import { Schema, TransactionStateEnum } from ".";
 import { Log } from "./log";
 import { Signature } from "./signature";
 
@@ -9,7 +9,17 @@ export type ListTransactionsOfTssResponseDataItem = {
     log: Log;
     metadata: object;
     number: number;
+    qr_code_data?: string;
     revision: number;
     schema: Schema;
     signature: Signature;
+    state: TransactionStateEnum;
+    time_end?: number;
+    time_start: number;
+    tss_id: string;
+    tss_serial_number: string;
+    _env: string;
+    _id: string;
+    _type: string;
+    _version: string;
 }
